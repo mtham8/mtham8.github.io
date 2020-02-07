139b3770c97345776444778d827c0720a1c28a2f
Michelle Tham <mtham101@gmail.com>
2020-02-07 12:49:18 -0800

    update footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -63,7 +63,7 @@ class Layout extends React.Component {
         <header>{header}</header>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}
+          © Michelle T {new Date().getFullYear()}
         </footer>
       </div>
     )
